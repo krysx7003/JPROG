ea082e55d8c2635e327be7155308690c1dd27d15
krysx7003 <krysx7003@gmail.com>
2024-01-10 18:06:13 +0100

    123Test

--- app/src/main/java/Zadanie_2/Frame.java
+++ app/src/main/java/Zadanie_2/Frame.java
@@ -2,17 +2,17 @@ package Zadanie_2;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ItemEvent;
-//import java.awt.event.ActionListener;
-//import java.awt.event.ActionEvent;
-import java.awt.event.ItemListener;
+import java.awt.event.*;
+import java.util.ArrayList;
 
-public class Frame extends JFrame {
+public class Frame extends JFrame  {
     Panel panel = new Panel();
     JTextField[] allFields = new JTextField[10];
+    JTextField[] allLabels = new JTextField[10];
     JCheckBox[] allCheckBoxs = new JCheckBox[10];
     JPanel checkBoxPanel = new JPanel();
     JPanel textPanel = new JPanel();
+    JPanel labelPanel = new JPanel();
     int[][] tabelOfValues = new int[2][10];
     
     public void redrawGraph(){
@@ -23,59 +23,120 @@ public class Frame extends JFrame {
         super("Zadanie 2");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pack();
-        setSize(800,800);
+        setSize(600,400);
         setLocation(200,200);
         setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
+        JTextField outField = new JTextField();
         //Create panel of text panels
         textPanel.setLayout(new GridLayout(0,1));
         //Create panel of check boxes
         checkBoxPanel.setLayout(new GridLayout(0,1));
+        labelPanel.setLayout(new GridLayout(0,1));
+        ArrayList<Color> colorWheel = panel.getColors();
         for(int i=0;i<10;i++){
-            c.fill = GridBagConstraints.NONE;
             final int index = i;
             //Fill tabelOfValues with 0
             tabelOfValues[0][i] = 0;
             tabelOfValues[1][i] = 0;
             //Create new text field 4 columns wide
             allFields[i] = new JTextField(4);
+            //Set background of text field to coolor from graph
+            allFields[i].setBackground(colorWheel.get(i));
+            allFields[i].addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    panel.setList(tabelOfValues);
+                    panel.repaint();
+                }
+            });
             textPanel.add(allFields[i]);
             //Create new check box
             allCheckBoxs[i] = new JCheckBox();
-            //Add a ItemListener to check box 
+            //Add a ItemListener to check box =
             allCheckBoxs[i].addItemListener(new ItemListener() {    
                 public void itemStateChanged(ItemEvent e) {
                     //If check box was just selected  
                     if(allCheckBoxs[index].isSelected()){
                             //Set values of tabelOfValues to reflect current state
-                            tabelOfValues[0][index] = 1;
-                            tabelOfValues[1][index] = Integer.parseInt(allFields[index].getText());
-                            //Redraw graph
-                            redrawGraph();
+                            try{
+                                tabelOfValues[1][index] = Integer.parseInt(allFields[index].getText());
+                                tabelOfValues[0][index] = 1;
+                            }catch(NumberFormatException x){                     
+                                outField.setText("Pole "+index+" jest puste");
+                            }    
                     //If check box was unselected
                         }else{
                             //Set "boolean" to false
                             tabelOfValues[0][index] = 0;
                         }     
-                      
                 }    
-             });
+            });
+            allCheckBoxs[i].addKeyListener(new KeyAdapter() {
+                @Override
+                public void keyPressed(KeyEvent e) {
+                    if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                        outField.setText("Wykres zostal przerysowany");
+                        panel.setList(tabelOfValues);
+                        panel.repaint();
+                    }
+                }
+            });
             checkBoxPanel.add(allCheckBoxs[i]);
+            allLabels[i] = new JTextField(2);
+            allLabels[i].setText("   "+(i+1)+".");
+            allLabels[i].setEditable(false);
+            labelPanel.add(allLabels[i]);
             
+            panel.getInputMap().put(KeyStroke.getKeyStroke("ENTER"), "enterAction");
+            panel.getActionMap().put("enterAction", enterAction);
             
         }
+
         //Give tabelOfValues to Panel 
         panel.setList(tabelOfValues);
         //Put graph in frame
         add(panel,c);
-        c.gridx = 2;
+        c.gridx = 3;
         //Put panel of 10 text panels in frame
         add(textPanel,c);
-        c.gridx = 1;
+        c.gridx = 2;
         //Put panel of 10 check boxes
         add(checkBoxPanel,c);
+        c.gridx = 1;
+        add(labelPanel,c);
+        c.gridx = 0;
+        c.gridy = 1;
+        c.fill  = 1;
+        outField.setEditable(false);
+        add(outField,c);
+        JButton redrawButton = new JButton("Redraw");
+        redrawButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                outField.setText("Wykres zostal przerysowany");
+                panel.setList(tabelOfValues);
+                panel.repaint();
+            }
+            
+        });
+        c.gridx = 1;
+        c.gridy = 1;
+        c.gridwidth = 3;
+        c.fill  = 3;
+        add(redrawButton,c);
         setVisible(true);
     }
+    Action enterAction = new AbstractAction() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("Enter został wciśnięty!");
+            panel.setList(tabelOfValues);
+            panel.repaint();
+
+        }
+    };
     
     
     
--- app/src/main/java/Zadanie_2/Panel.java
+++ app/src/main/java/Zadanie_2/Panel.java
@@ -1,6 +1,7 @@
 package Zadanie_2;
 
 import java.awt.*;
+
 import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.JPanel;
@@ -8,6 +9,7 @@ import javax.swing.JPanel;
 public class Panel extends JPanel {
     static ArrayList<Color> colorWheel = new ArrayList<>();
     int[][] list;
+
     public Panel() {
         setPreferredSize(new Dimension(400, 200));
         //After initialization get 10 random Colors 
@@ -23,6 +25,9 @@ public class Panel extends JPanel {
         float blue = rand.nextFloat();
         return new Color(red,green,blue);
     }
+    public ArrayList<Color> getColors(){
+        return colorWheel;
+    }
     public void setList(int[][] listToSet){
         //Set list of "booleans" and ints
         this.list = listToSet;
@@ -30,7 +35,7 @@ public class Panel extends JPanel {
     public boolean checkIfListEmpty(){
         //Go through whole array and check if there are numbers in it
         for(int i=0;i<10;i++){
-            if(list[1][i]==0){
+            if(list[1][i]!=0){
                 //If there are numbers return false
                 return false;
             }
@@ -42,8 +47,8 @@ public class Panel extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         //Draw simple guide line
-        g.drawLine(0, 199, 400, 199);
-        if(checkIfListEmpty()){
+        
+        if(!checkIfListEmpty()){
             int newMax = 0;
             int ammountOfBoxes = 0;
             for(int i=0;i<10;i++){
@@ -56,19 +61,25 @@ public class Panel extends JPanel {
             }
             //Set width of each column
             int width = 360/ammountOfBoxes;
+            int j = 0;
             for(int i=0;i<10;i++){
                 //Setsone of the Colors in colorWheel
                 g.setColor(colorWheel.get(i));
                 //If box is checked draws rectangle
                 if(list[0][i]==1){
                     //Calculate height of the column
-                    double height =list[1][i]/newMax;
-                    g.fillRect(width*i+20, 10, width, (int)height);
+                    double height =(double)list[1][i]/newMax;
+                    height = height*200;
+                    g.fillRect(width*j+20, 200-(int)height, width, (int)height);
+                    j++;
                 }
                 
             }
         }
-        
+
+        g.setColor(Color.BLACK);
+        //Draw simple guide line
+        g.drawRect(0, 0, 399, 199);
         
     }
 
